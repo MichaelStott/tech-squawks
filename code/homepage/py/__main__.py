1e9e754452cc87d8f229ce9df5a39354245c044f
MichaelStott <stottmichael2@gmail.com>
2022-07-31 15:31:24 -0400

    Updating python example

--- code/homepage/py/__main__.py
+++ code/homepage/py/__main__.py
@@ -1,66 +1,23 @@
-"""NOTE: Because Pulumi Crosswalk is only available for Typescript and Javascript,
-the Python and Golang based examples are significantly larger.
-"""
-
-import json, os, mimetypes
-
 import pulumi
-from pulumi import export, FileAsset, ResourceOptions, Output
-import pulumi_aws as aws
-from pulumi_aws import s3, lambda_, apigateway
+import pulumi_aws
 
+from lambda_util import create_python_lambda
 
-# Create Lambda permissions.
-lambda_role = aws.iam.Role("apiGatewayLambdaRole", 
-    assume_role_policy=json.dumps({
-        "Version": "2012-10-17",
-        "Statement": [{
-                "Action": "sts:AssumeRole",
-                "Principal": {
-                    "Service": "lambda.amazonaws.com",
-                },
-                "Effect": "Allow",
-                "Sid": "",
-            }]
-    }))
-role_policy_attachment = aws.iam.RolePolicyAttachment("lambdaRoleAttachment",
-    role=lambda_role,
-    policy_arn=aws.iam.ManagedPolicy.AWS_LAMBDA_BASIC_EXECUTION_ROLE)
-
-# Provision Lambda function which will be invoked upon an http request.
 LAMBDA_SOURCE = 'lambda.py'
 LAMBDA_PACKAGE = 'lambda.zip'
 LAMBDA_VERSION = '1.0.0'
-os.system('zip %s %s' % (LAMBDA_PACKAGE, LAMBDA_SOURCE))
 
-# Create an AWS resource (S3 Bucket)c
-bucket = s3.Bucket('lambda-api-gateway-example')
-
-mime_type, _ = mimetypes.guess_type(LAMBDA_PACKAGE)
-obj = s3.BucketObject(
-            LAMBDA_VERSION+'/'+LAMBDA_PACKAGE,
-            bucket=bucket.id,
-            source=FileAsset(LAMBDA_PACKAGE),
-            content_type=mime_type
-            )
-
-lambda_function = lambda_.Function(
-    'ServerlessExample',
-    s3_bucket=bucket.id,
-    s3_key=LAMBDA_VERSION+'/'+LAMBDA_PACKAGE,
-    handler="lambda.handler",
-    runtime="python3.7",
-    role=lambda_role.arn,
-)
+# Provision Lambda function which will be invoked upon an http request.
+lambda_function = create_python_lambda(LAMBDA_SOURCE, LAMBDA_PACKAGE, LAMBDA_VERSION)
 
 # Give API Gateway permissions to invoke the Lambda
-lambda_permission = aws.lambda_.Permission("lambdaPermission", 
+lambda_permission = pulumi_aws.lambda_.Permission("lambdaPermission", 
     action="lambda:InvokeFunction",
     principal="apigateway.amazonaws.com",
     function=lambda_function)
 
 # Set up the API Gateway
-apigw = aws.apigatewayv2.Api("httpApiGateway", 
+apigw = pulumi_aws.apigatewayv2.Api("httpApiGateway", 
     protocol_type="HTTP",
     route_key="GET /",
     target=lambda_function.invoke_arn)
