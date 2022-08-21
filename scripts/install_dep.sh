47fdc683003bc992497785dd68053cb2f7c82e41
MichaelStott <stottmichael2@gmail.com>
2022-08-21 16:49:34 -0400

    Install project dependency

---
+++ scripts/install_dep.sh
@@ -0,0 +1,13 @@
+
+if [[ $name == *py ]] 
+then
+  pip install -r requirements.txt -t $WORKING_DIR
+elif [[ $name == *ts || $name == *js]] 
+then
+  npm i --prefix $WORKING_DIR
+elif [[ $name == *go]] 
+then
+  echo "Nothing to do."
+else
+  echo "Cannot detect language from directory: " + $WORKING_DIR; 
+fi
