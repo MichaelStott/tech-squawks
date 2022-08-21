63181cbbfe3f3c98c34810199790e5f2e92f930f
MichaelStott <stottmichael2@gmail.com>
2022-08-21 14:50:29 -0400

    Updating ci-cd script

--- scripts/enumerate_pulumi.py
+++ scripts/enumerate_pulumi.py
@@ -1,10 +1,16 @@
+""" Script used by CI/CD process to determine which directories to test.
+"""
 
 class PulumiEnumerator():
 
     def __init__(self):
+        """ Initialize enumerators. Determine which directories to test.
+        """
         self.project_list_file = "pulumi_project_list.txt"
 
     def get_pulumi_projects(self):
+        """ Get Pulumi project directories.
+        """
         results = []
         with open(self.project_list_file) as project_file:
             for line in project_file:
