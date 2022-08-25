a1c90af0eb738812fd1d26c09b00f2881f764e9a
MichaelStott <stottmichael2@gmail.com>
2022-08-25 18:38:02 -0400

    Auto detect pulumi projects.

--- scripts/enumerate_pulumi.py
+++ scripts/enumerate_pulumi.py
@@ -1,22 +1,34 @@
 """ Script used by CI/CD process to determine which directories to test.
 """
 
+import os
+from pathlib import Path
+
 class PulumiEnumerator():
+
+    DEFAULT_PROJECT_DIR = "code"
 
     def __init__(self):
         """ Initialize enumerators. Determine which directories to test.
         """
-        self.project_list_file = "pulumi_project_list.txt"
+        self.project_dir = os.getenv("PROJECT_DIR", self.DEFAULT_PROJECT_DIR)
+        self.blacklisted = self._get_ignore_dirs()
+        
+    def _get_ignore_dirs(self):
+        """ Determine which directories to ignore.
+        """
+        blacklisted = os.getenv("BLACKLISTED_DIRS", "")
+        return [x.strip() for x in blacklisted.split(",")]
 
     def get_pulumi_projects(self):
         """ Get Pulumi project directories.
         """
-        results = []
-        with open(self.project_list_file) as project_file:
-            for line in project_file:
-                if line:
-                    results.append(line.removesuffix("\n"))
-        return results
+        results = set()
+        for path in Path(self.project_dir).rglob('Pulumi.*.yaml'):
+            path_str = str(path)
+            if "node_modules" not in path_str:
+                results.add(path_str)
+        return list(results)
 
 if __name__ == "__main__":
     pulumi_enum = PulumiEnumerator()
