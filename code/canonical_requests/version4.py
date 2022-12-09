870e1c9f23d871dbf5140b61675d7db89eb97e58
MichaelStott <stottmichael2@gmail.com>
2022-12-08 19:23:49 -0500

    initial canonical requests

--- code/canonical_requests/version4.py
+++ code/canonical_requests/version4.py
@@ -13,6 +13,7 @@ current_credentials = credentials.get_frozen_credentials()
 AWS_ACCESS_KEY_ID = current_credentials.access_key
 AWS_SECRET_ACCESS_KEY = current_credentials.secret_key
 
+METHOD = "POST"
 SIGNING_ALGORITHM = "AWS4-HMAC-SHA256"
 AMAZON_TARGET = "AmazonSSM.GetParameter"
 CONTENT_TYPE = "application/x-amz-json-1.1"
@@ -20,6 +21,11 @@ PARAMETER_NAME = "SlawekTestParam"
 SERVICE = "ssm"
 HOST = "ssm.us-west-2.amazonaws.com"
 REGION = "us-west-2"
+
+SIGNED_HEADERS = "content-type;host;x-amz-date;x-amz-target"
+
+CANONICAL_URI = "/"
+CANONICAL_QUERY_STRING = ""
 
 def hex_encode(input: str):
     return str(binascii.hexlify(bytes(input,"ascii")))
@@ -51,6 +57,13 @@ def get_credential_scope(date_stamp: str):
 def get_endpoint():
     return "https://{}/".format(HOST)
 
+def get_canonical_headers(amzn_date):
+    headers = "content-type:" + CONTENT_TYPE + "\nhost:" + HOST + "\nx-amz-date:" + amzn_date + "\nx-amz-target:" + AMAZON_TARGET + "\n"
+    return headers
+
+def get_canonical_requests(canonical_headers):
+    return [METHOD, CANONICAL_URI, CANONICAL_QUERY_STRING, canonical_headers, SIGNED_HEADERS, payload_hash].join("\n")
+
 if __name__ == "__main__":
     hex_hash = hex_encode(compute_sha256_hash("hello world!"))
     print ("Comoputed Hash: " + str(hex_hash))
@@ -64,8 +77,11 @@ if __name__ == "__main__":
     print("amzn_time: " + amazon_timestamp)
     print("req_time: " + req_timestamp)
 
+    headers = get_canonical_headers(amazon_timestamp)
+    canoniocal_request = get_canonical_requests(headers)
+    
     request_paramters = '{"Name":"' + PARAMETER_NAME + '","WithDecryption":true}'
-
+    payload_hash = sign(request_paramters)
     scope = get_credential_scope(req_timestamp)
     print ("scope: " + scope)
     # Perform AWS API call
