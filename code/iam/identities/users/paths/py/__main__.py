b53064666e7e1c4eaf8aeb960f3c52997b3989d4
MichaelStott <stottmichael2@gmail.com>
2023-08-07 18:38:06 -0400

    Fixing python examples

--- code/iam/identities/users/paths/py/__main__.py
+++ code/iam/identities/users/paths/py/__main__.py
@@ -2,6 +2,6 @@ import pulumi
 from pulumi_aws import iam
 
 # Create IAM user with long-lived access credentials
-user1 = iam.User("techsquawks-user", name="techsquawks-user1", path="/example/path/1/")
-user2 = iam.User("techsquawks-user", name="techsquawks-user2", path="/example/path/2/")
-user2a = iam.User("techsquawks-user", name="techsquawks-user2a", path="/example/path/2/")
+user1 = iam.User("techsquawks-user-1", name="techsquawks-user1", path="/example/path/1/")
+user2 = iam.User("techsquawks-user-2a", name="techsquawks-user2", path="/example/path/2/")
+user2a = iam.User("techsquawks-user-2b", name="techsquawks-user2a", path="/example/path/2/")
