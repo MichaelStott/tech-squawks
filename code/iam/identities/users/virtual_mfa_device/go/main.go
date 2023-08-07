0fedabeaac93ba04d27f41888f403d47217112c3
MichaelStott <stottmichael2@gmail.com>
2023-08-07 18:33:18 -0400

    Updating golang example

--- code/iam/identities/users/virtual_mfa_device/go/main.go
+++ code/iam/identities/users/virtual_mfa_device/go/main.go
@@ -8,21 +8,21 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create IAM user with long-lived access credentials
-		user1, err := iam.NewUser(ctx, "techsquawks-user1", &iam.UserArgs{
+		_, err := iam.NewUser(ctx, "techsquawks-user1", &iam.UserArgs{
 			Name: pulumi.String("techsquawks-user1"),
 			Path: pulumi.String("/example/path/1/"),
 		})
 		if err != nil {
 			return err
 		}
-		user2, err := iam.NewUser(ctx, "techsquawks-user2", &iam.UserArgs{
+		_, err = iam.NewUser(ctx, "techsquawks-user2", &iam.UserArgs{
 			Name: pulumi.String("techsquawks-user2"),
 			Path: pulumi.String("/example/path/2/"),
 		})
 		if err != nil {
 			return err
 		}
-		user2a, err := iam.NewUser(ctx, "techsquawks-user2a", &iam.UserArgs{
+		_, err = iam.NewUser(ctx, "techsquawks-user2a", &iam.UserArgs{
 			Name: pulumi.String("techsquawks-user2a"),
 			Path: pulumi.String("/example/path/2/"),
 		})
